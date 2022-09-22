522b7d0ef3b71f46f558d4fde824e99f573927d3
Csuka Dávid <davidcsuka8@gmail.com>
2022-09-22 19:11:56 +0200

    Homework

---
+++ Gyakoralat2.py
@@ -0,0 +1,11 @@
+# Létrehoz egy változót melynek értéke 20 és neve szelesseg 
+szelesseg = 20
+# Létrehoz egy változót melynek értéke 5*9.3(46.5) és neve magassag 
+magassag = 5 * 9.3
+# Összesoroza a két változót és kiirja 
+print(szelesseg*magassag)
+
+a = 3
+b = 5
+c = 7
+print(a-b/c)
---
+++ Gyakorlat3.py
@@ -0,0 +1,10 @@
+j = 0
+while j < 20:
+    j=j+1
+    print(j*7)
+
+euro = 1
+while euro < 16385:
+  print('{0} euro:{1:.2f} dollar'.format(euro,round(euro*1.65,2)))
+  euro=euro*2
+    
