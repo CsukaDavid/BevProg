3c85eab4035bd9a6ea374a7d51829d96e366fd23
Csuka Dávid <davidcsuka8@gmail.com>
2022-10-06 20:55:23 +0200

    Update Homework_3_1.py

--- Gyakorlás/Homework_3_1.py
+++ Gyakorlás/Homework_3_1.py
@@ -1,24 +1,27 @@
 def foo(text,find,place):
     print(place+" "+find)
-    number =text.rfind(find)
-    text = text[0:number]+place+" "+find+text[number+len(find):len(text)]
-    print(text)
+    number =text.find(find)
+    if (number==-1):
+        return
+    else:
+        text = text[0:number]+place+" "+find+text[number+len(find):len(text)]
+        print(text)
 
 def main():
-    print("Írj be egy mondatot:")
+    print("Írj be egy mondatot: ",end="")
     text =str(input())
-    print("Szeretnél valamelyik szó elé valamit rakni?(Y/N)")
+    print("Szeretnél valamelyik szó elé valamit rakni?(Y/N)",end=" ")
     character=str(input())
     if (character=="N"):
         print(text)
     else:
-        print("Keresendö szó: ")
+        print("Keresendö szó: ",end="")
         find = str(input())
-        print("A szó elé mit szeretnél rakni :")
+        print("A szó elé mit szeretnél rakni :",end="")
         place= str(input())
         foo(text,find,place)
     print("""\nHa jönn Szondi.akkor kéne nézni telefonhoz gyors töltött
-,kábelel,adapterrel. a Redmi Note 9 Prohoz hajtogatos tok és kettö db PEREMMENTES(Keretmentes) üvegfólia(Note 9 prohoz,
+,kábelel,adapterrel. a Redmi Note 9 Prohoz és kettö db PEREMMENTES(Keretmentes) üvegfólia(Note 9 prohoz,
 Huawei Mate 20 Litehoz) Emag-rol , Code Vein Deluxe edition game 4660huf OKTOBER 10 85% DARK SOULS GAME WIFU SOULS. MINÉL HAMARABB\n""")
 
 if __name__=="__main__":
